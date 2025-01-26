deb9bd0a1fbb7579f9b0107839cdcf0fc2987ee8
Bennykuu <Benjamin.Weiss01@gmx.de>
2025-01-27 00:05:46 +0100

    Add files via upload

---
+++ Ascii_symple.py
@@ -0,0 +1,32 @@
+from PIL import Image, ImageDraw, ImageFont
+
+picture = input("which image do you wanna us:")
+
+image = Image.open(picture).convert("L")
+
+factor = 8
+ASCII = [' ', '.', ':', '=', 'c', 'o', '?', '#', '%', '@']
+
+# Downsampling: Bild auf ein Achtel der Größe verkleinern ( 8 x 8 )
+downsized = image.resize((image.size[0] // factor, image.size[1] // factor), Image.Resampling.LANCZOS)
+
+# Neues Bild erstellen für die Ausgabe
+output_image = Image.new("RGB", image.size, "black")
+draw = ImageDraw.Draw(output_image)
+
+pixels = downsized.load() # Pixel-Access-Objekt
+for y in range(downsized.height):
+    for x in range(downsized.width):
+        luminance = pixels[x, y] / 255  # Normalisiere auf Bereich [0, 1]
+
+        uv_x = (x % factor) / factor + luminance # video hilfe
+        uv_y = (y % factor) / factor # video hilfe
+
+        index = min(int(luminance * (len(ASCII)-1)), len(ASCII) - 1) # [0,9]
+        symbol = ASCII[index]
+
+        draw.text((x * factor, y * factor), symbol, fill="white")
+
+output_image.show()
+
+
